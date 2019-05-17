9b95e2c88f27bc29f61ad0990a91d96c5bab1ce4
Menonro <nitayd2001@gmail.com>
2019-05-17 15:11:07 +0600

    0.1.7

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const path = require('path')
+const serveStatic = require('serve-static')
+
 const app = express()
 
 async function start() {
-  app.use('/', express.static(path.resolve(__dirname, 'build')));
-  console.log(app)
+  app.use(serveStatic(path.join(__dirname, 'build')))
   return app
 }
 
