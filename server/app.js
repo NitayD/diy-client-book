76e1d9e7b9ab6570628be34ce9d61e6895a28616
Menonro <nitayd2001@gmail.com>
2019-05-17 15:07:03 +0600

    0.1.6

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express()
 
 async function start() {
-  app.use(express.static(path.resolve(__dirname, 'build')));
+  app.use('/', express.static(path.resolve(__dirname, 'build')));
   console.log(app)
   return app
 }
