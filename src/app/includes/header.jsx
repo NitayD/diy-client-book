1453fa685eb09060a42498d74ada797b6a638c62
Menonro <nitayd2001@gmail.com>
2019-05-23 10:59:43 +0600

    0.4.0

--- src/app/includes/header.jsx
+++ src/app/includes/header.jsx
@@ -11,32 +11,32 @@ class Header extends Component {
     return (
       <Navbar bg="dark" variant="dark" expand="lg">
         <Container>
-            <Navbar.Brand><Link to="/" style={{fontWeight: 900}}>DIY Мастер-Классы</Link></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" className="ml-auto"/>
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav>
-                <Nav.Item>
-                  <Link to="/" className="nav-link">Главная</Link>
-                </Nav.Item>
-                <NavDropdown title="Мастер-Классы">
-                  <Link to="/" className="dropdown-item">Предстоящие МК</Link>
-                  <Link to="/mc/old" className="dropdown-item">Прошедшие МК</Link>
-                </NavDropdown>
-                <NavDropdown title="+Добавить ...">
-                  <Link to="/create/mc" className="dropdown-item">Мастер-Класс</Link>
-                  <Link to="/create/project" className="dropdown-item">Проект</Link>
-                </NavDropdown>
-                <Nav.Item>
-                  <Link to="/create/inventory" className="nav-link">Инвентаризационный учёт</Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Link to="/clients" className="nav-link">Список взрослых</Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href='https://diystudio.kz/'>Внешний сайт</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Navbar.Collapse>
+          <Navbar.Brand><Link to="/" style={{fontWeight: 900}}>DIY Мастер-Классы</Link></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className="ml-auto"/>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Item>
+                <Link to="/" className="nav-link">Главная</Link>
+              </Nav.Item>
+              <NavDropdown title="Мастер-Классы">
+                <Link to="/" className="dropdown-item">Предстоящие МК</Link>
+                <Link to="/mc/old" className="dropdown-item">Прошедшие МК</Link>
+              </NavDropdown>
+              <NavDropdown title="+Добавить ...">
+                <Link to="/create/mc" className="dropdown-item">Мастер-Класс</Link>
+                <Link to="/create/project" className="dropdown-item">Проект</Link>
+              </NavDropdown>
+              <Nav.Item>
+                <Link to="/create/inventory" className="nav-link">Инвентаризационный учёт</Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Link to="/clients" className="nav-link">Список взрослых</Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href='https://diystudio.kz/'>Внешний сайт</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     );
